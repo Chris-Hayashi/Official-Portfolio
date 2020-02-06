5aaf19127f62fcc72c3d703ce314458d009cd6e6
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-05 17:11:30 -0800

    installed npm packages and added server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static("./app/public"));
+
+
+var htmlRoutes = require("./app/routes/html-routes");
+app.use(htmlRoutes);
+
+
+// app.use(console.log(SHA256("Message")));
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});s
